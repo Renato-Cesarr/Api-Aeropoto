170bb368a86cf8e2669cac4eabf6d85136c63978
Isabella Bellinazzi <ibellinazzi@almavivasolutions.com.br>
2025-01-09 17:17:06 +0000

    Revert "Merge branch 'dev_structure' into 'master'"
    This reverts merge request !1

--- src/main/java/com/renato/aeroporto_api/exception/AeroportoNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.renato.aeroporto_api.exception;
-
-public class AeroportoNotFoundException extends RuntimeException {
-    public AeroportoNotFoundException(String message) {
-        super(message);
-    }
-}
--- src/main/java/com/renato/aeroporto_api/exception/AviaoNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.renato.aeroporto_api.exception;
-
-public class AviaoNotFoundException extends RuntimeException {
-    public AviaoNotFoundException(String message) {
-        super(message);
-    }
-}
